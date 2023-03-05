7ee9b5f81a097df4b99b42abd866ceaecbd76849
MarekSzklanny <szklamarek@gmail.com>
2023-03-05 20:13:01 +0100

    Program wybierający pracowników z najwyższymi ocenami

---
+++ 6.-Zadanie-domowe-A/6.-Zadanie-domowe-A/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace _6._Zadanie_domowe_A
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public int Age { get; private set; }
+
+        public int Result
+        {
+            get
+            {
+                return score.Sum();
+            }
+        }
+        public void AddScore(int number)
+        {
+            this.score.Add(number);
+        }
+    }
+}
---
+++ 6.-Zadanie-domowe-A/6.-Zadanie-domowe-A/Program.cs
@@ -0,0 +1,51 @@
+﻿using _6._Zadanie_domowe_A;
+
+Employee employee1 = new Employee("Jan", "Kowalski", 25);
+Employee employee2 = new Employee("Anna", "Zawada", 30);
+Employee employee3 = new Employee("Andrzej", "Nowak", 29);
+
+employee1.AddScore(1);
+employee1.AddScore(7);
+employee1.AddScore(2);
+employee1.AddScore(5);
+employee1.AddScore(1);
+
+employee2.AddScore(4);
+employee2.AddScore(5);
+employee2.AddScore(9);
+employee2.AddScore(9);
+employee2.AddScore(8);
+
+employee3.AddScore(9);
+employee3.AddScore(7);
+employee3.AddScore(5);
+employee3.AddScore(3);
+employee3.AddScore(9);
+
+List<Employee> employees = new List<Employee>()
+    {
+        employee1, employee2, employee3
+    };
+var maxResult = 0;
+
+Employee employeeWithMaxResult = null;
+
+foreach (var employee in employees)
+{
+    if (employee1.Result > maxResult)
+    {
+        employeeWithMaxResult = employee1;
+        maxResult = employee1.Result;
+    }
+    else if (employee2.Result > maxResult)
+    {
+        employeeWithMaxResult = employee2;
+        maxResult = employee2.Result;
+    }
+    else if (employee3.Result > maxResult)
+    {
+        employeeWithMaxResult = employee3;
+        maxResult = employee3.Result;
+    }
+}
+Console.WriteLine($"Najwięcej punktów: {maxResult} zdobywa {employeeWithMaxResult.Name} {employeeWithMaxResult.Surname}, lat: {employeeWithMaxResult.Age}");
